b9fd767d3daff12834574809f9017c59ef7dd88f
M-Anilkumar <108669932+M-Anilkumar@users.noreply.github.com>
2022-07-25 11:07:38 +0530

    update

--- 2022-07-23_2/folder.sh
+++ 2022-07-23_2/folder.sh
@@ -5,8 +5,7 @@ a=$1
 b=$(echo $a | sed "s/\///")
 #dir_name=$(echo $b | awk '{print $1}')
 #touch file1_sam_re_k_l_m_l
-#find / -name "anil" > file1_sam_re_k_l_m_l | grep -v "find:"
-#echo $dir_name
+#find / -name "anil" | grep -v "permission denied" > file1_sam_re_k_l_m_l
 
 rm -r $BASE_DIR/$b
 
